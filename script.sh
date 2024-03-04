f3bf9bac6f4bb49d8138bae9ff0c25183bd4daab
niassais <162188842+niassais@users.noreply.github.com>
2024-03-04 21:38:48 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/bash
+
+#Install and launch Tor Browser - Root
 echo zeb yetgha kilbanana
 wget https://www.torproject.org/dist/torbrowser/13.0.10/tor-browser-linux-x86_64-13.0.10.tar.xz
 tar -xf tor-browser-linux-x86_64-13.0.10.tar.xz
