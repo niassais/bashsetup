ded1bc19dbd33189eaebe12b026b29376a7abbbb
niassais <162188842+niassais@users.noreply.github.com>
2024-03-04 20:33:01 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/bash
 echo zeb yetgha kilbanana
+wget https://www.torproject.org/dist/torbrowser/13.0.10/tor-browser-linux-x86_64-13.0.10.tar.xz
+tar -xf tor-browser-linux-x86_64-13.0.10.tar.xz
+./tor-browser/start-tor-browser.desktop
