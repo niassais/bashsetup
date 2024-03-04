54a2d702fb3b929fea10a8ba8fdb7cbf40789fe0
niassais <162188842+niassais@users.noreply.github.com>
2024-03-04 21:34:44 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,5 +5,6 @@ tar -xf tor-browser-linux-x86_64-13.0.10.tar.xz
 cd tor-browser
 cd Browser
 rm start-tor-browser
-mv /bashsetup/setup-tor-browser ./
+mv ./../../start-tor-browser ./
+chmod u+x start-tor-browser 
 ./start-tor-browser
